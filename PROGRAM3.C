19a7bf19bfc32a521708bb35d3af239cdc9470d5
shrutimishra210 <shrutimishra2105@gmail.com>
2026-03-19 08:19:49 +0530

    Create PROGRAM3.C

---
+++ PROGRAM3.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h>
+//define union
+union student     
+{
+int rollno;
+char name[20];
+char department[40];
+int year_of_joining;
+float score;
+};                 
+void main()
+{
+union student s;  //declare union variable  
+
+//input data
+printf("Enter roll number:");
+scanf("%d",&s.rollno);
+printf("Enter name:");
+scanf("%s[^\n]",&s.name); //to read full name with spaces
+printf("Enter department:");
+scanf("%s[^\n]",&s.department);
+printf("Enter the year of joining:");
+scanf("%d",&s.year_of_joining);
+printf("Enter the score:");
+scanf("%f",&s.score);
+//display data
+printf("\n----STUDENT DETAILS----\n");
+printf("Roll number:%d\n",s.rollno);
+printf("Name:%s\n",s.name);
+printf("Department:%s\n",s.department);
+printf("Year of joining:%d\n",s.year_of_joining);
+printf("Score:%.2f\n",s.score);
+getch();
+
+}
